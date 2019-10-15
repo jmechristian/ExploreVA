967013dcbd471cda133027a17158870b0eab052d
JMEChristian <jmechristian@gmail.com>
2019-10-15 19:15:52 -0400

    sdsdsd

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Auth';
+import { useMediaQuery } from 'react-responsive';
 import app from '../firebase';
 
 const Header = () => {
@@ -13,19 +14,31 @@ const Header = () => {
     }
   };
 
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  let headerContent = null;
+  if (!isMobile) {
+    headerContent = (
+      <>
+        <div className="text-white text-xs font-bold uppercase">
+          Hello, {currentUser.displayName}
+        </div>
+        <div>
+          <button
+            onClick={signOutHandler}
+            className="text-white text-xs font-bold uppercase"
+          >
+            Sign Out
+          </button>
+        </div>
+      </>
+    );
+  } else {
+    headerContent = null;
+  }
+
   return (
     <div className="flex justify-between items-center py-6 px-10 w-full bg-blue-400 ">
-      <div className="text-white text-xs font-bold uppercase">
-        Hello, {currentUser.displayName}
-      </div>
-      <div>
-        <button
-          onClick={signOutHandler}
-          className="text-white text-xs font-bold uppercase"
-        >
-          Sign Out
-        </button>
-      </div>
+      {headerContent}
     </div>
   );
 };
