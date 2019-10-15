cf2462416fc10a3ee57cc7e533858b791d87a445
JMEChristian <jmechristian@gmail.com>
2019-10-15 17:12:48 -0400

    first draft

--- src/components/Blog/noContent.js
+++ src/components/Blog/noContent.js
@@ -4,12 +4,19 @@ import { faWalking } from '@fortawesome/free-solid-svg-icons';
 
 const NoContent = () => {
   return (
-    <div className="px-16 text-center">
-      <div>
-        <FontAwesomeIcon icon={faWalking} size="6x" />
+    <div className="flex flex-col items-center text-center">
+      <div className="flex mb-4">
+        <FontAwesomeIcon
+          icon={faWalking}
+          size="6x"
+          className="font-color-secondary"
+        />
       </div>
-      <div className="text-4xl font-extrabold">
+      <div className="text-4xl font-extrabold flex w-3/4">
         Click a pin to see what we've been up to!
+      </div>
+      <div className="text-2xl font-extrabold flex w-3/4">
+        Use your mouse to zoom and move around the map.
       </div>
     </div>
   );
